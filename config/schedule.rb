8c9848a9dd8da3f844595cbc2a48858fc448ac6d
Raghavendra Joshi <rjoshi@aasalabs.com>
2021-08-05 17:41:34 +0530

    Added cronjob details for Occupancy report generator

---
+++ config/schedule.rb
@@ -0,0 +1,26 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+set :output, "log/cron.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+job_type :rbenv_runner, %Q{export PATH=$HOME/.rbenv/bin:$PATH; eval "$(rbenv init -)"; cd :path && bundle exec rails runner :task --silent :output }
+
+every 1.day, at: '1:38 pm' do
+  rbenv_runner "OccupancyReportGenerator.generate"
+end
+
+# Learn more: http://github.com/javan/whenever

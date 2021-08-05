b9cccda54f4af1f4c098438401bcfffd1bfdd9b7
Raghavendra Joshi <rjoshi@aasalabs.com>
2021-08-05 19:16:11 +0530

    Converted tabs to spaces

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,50 +1,50 @@
 class Reservation < ApplicationRecord
 
-	EMAIL_FORMAT_REG_EXP = /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/i
-	PHONE_NUMBER_REG_EXP = /((\+*)((0[ -]+)*|(91 )*)(\d{12}+|\d{10}+))|\d{5}([- ]*)\d{6}/
+  EMAIL_FORMAT_REG_EXP = /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/i
+  PHONE_NUMBER_REG_EXP = /((\+*)((0[ -]+)*|(91 )*)(\d{12}+|\d{10}+))|\d{5}([- ]*)\d{6}/
 
-	# Associations
-	belongs_to :room_group
+  # Associations
+  belongs_to :room_group
 
-	# Validations
-	validates 	:reference_code,
-							:status,
-							:site_code,
-							:sales_code,
-							:external_reservation_id,
-							:user_name,
-							:user_phone_number,
-							:user_prefecture,
-							:remarks,
-							:payment_type,
-							:currency,
-							:user_email,
-							presence: true
+  # Validations
+  validates   :reference_code,
+              :status,
+              :site_code,
+              :sales_code,
+              :external_reservation_id,
+              :user_name,
+              :user_phone_number,
+              :user_prefecture,
+              :remarks,
+              :payment_type,
+              :currency,
+              :user_email,
+              presence: true
 
-	validates	:user_email,
-						format: { with: EMAIL_FORMAT_REG_EXP }
-	
-	validates	:user_phone_number,
-						format: { with: PHONE_NUMBER_REG_EXP }													
+  validates :user_email,
+            format: { with: EMAIL_FORMAT_REG_EXP }
+  
+  validates :user_phone_number,
+            format: { with: PHONE_NUMBER_REG_EXP }                          
 
-	
-	validates		:accommodation_fee,
-							:service_fee,
-							:service_fee_for_cancellation,
-							:service_fee_rate,
-							:sales_fee,
-							:cleaning_fee,
-							:discount_by_coupon,
-							:consumption_tax,
-							:local_tax,
-							:total_price,
-							:cancel_fee,
-							:local_total_price,
-							:total_stay_adults,
-							:total_stay_children,
-							:total_stay_infants,
-							numericality: { greater_than_or_equal_to: 0 }
-	
-	validates		:checkin_time,
-							numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 23 }
+  
+  validates   :accommodation_fee,
+              :service_fee,
+              :service_fee_for_cancellation,
+              :service_fee_rate,
+              :sales_fee,
+              :cleaning_fee,
+              :discount_by_coupon,
+              :consumption_tax,
+              :local_tax,
+              :total_price,
+              :cancel_fee,
+              :local_total_price,
+              :total_stay_adults,
+              :total_stay_children,
+              :total_stay_infants,
+              numericality: { greater_than_or_equal_to: 0 }
+  
+  validates   :checkin_time,
+              numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 23 }
 end
--- app/models/room_group.rb
+++ app/models/room_group.rb
@@ -1,29 +1,29 @@
 class RoomGroup < ApplicationRecord
 
-	# Validations
-	validates 	:room_title,
-							:house_rule,
-							:house_manual,
-							:description,
-							:room_type,
-							:law_type,
-							presence: true
+  # Validations
+  validates   :room_title,
+              :house_rule,
+              :house_manual,
+              :description,
+              :room_type,
+              :law_type,
+              presence: true
 
-	validates		:order,
-							:maximum_guests,
-							:beds,
-							:bedrooms,
-							:bathrooms,
-							:bathtubs,
-							:base_inventories,
-							numericality: { only_integer: true, greater_than: 0 }
+  validates   :order,
+              :maximum_guests,
+              :beds,
+              :bedrooms,
+              :bathrooms,
+              :bathtubs,
+              :base_inventories,
+              numericality: { only_integer: true, greater_than: 0 }
 
-	validates		:size,
-							numericality: { greater_than: 0 }								
+  validates   :size,
+              numericality: { greater_than: 0 }               
 
-	validates		:is_shared_bath,
-							:is_open,
-							:is_edit_complete,
-							inclusion: { in: [true, false] }
+  validates   :is_shared_bath,
+              :is_open,
+              :is_edit_complete,
+              inclusion: { in: [true, false] }
 
 end

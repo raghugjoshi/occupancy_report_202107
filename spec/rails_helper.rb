acb72f2c1b633ed725347d585a2c40b447e00d76
Raghavendra Joshi <rjoshi@aasalabs.com>
2021-08-05 17:34:40 +0530

    VCR config is commented

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+
 ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
@@ -58,7 +59,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.strategy = [:truncation, only: %w[room_groups rooms reservations reservation_assignments]]
   end
 
   config.before(:each) do
@@ -76,9 +77,9 @@ RSpec.configure do |config|
   config.include ActiveJob::TestHelper
   config.include ActiveSupport::Testing::TimeHelpers
 
-  VCR.configure do |config|
-    config.cassette_library_dir = "spec/vcr"
-    config.hook_into :webmock
-    config.allow_http_connections_when_no_cassette = true
-  end
+  # VCR.configure do |config|
+  #   config.cassette_library_dir = "spec/vcr"
+  #   config.hook_into :webmock
+  #   config.allow_http_connections_when_no_cassette = true
+  # end
 end
